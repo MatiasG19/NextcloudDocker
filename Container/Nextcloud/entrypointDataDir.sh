f151b2d071a3d4cdd65b042b8e13993c438df915
MatiasG19 <m.galusek@gmail.com>
2024-10-09 22:54:21 +0200

    Add echo

--- Container/Nextcloud/entrypointDataDir.sh
+++ Container/Nextcloud/entrypointDataDir.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ ! -z "${NEXTCLOUD_DATA_DIR}" ]; then
+    echo Custom data directory: $NEXTCLOUD_DATA_DIR 
     mkdir -p $NEXTCLOUD_DATA_DIR
     chown -R www-data:www-data $NEXTCLOUD_DATA_DIR
 fi
